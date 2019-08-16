76d88c9e29fe1ab96fe8883a856e411280265520
Saqib Malik <saqib.s.malik12@gmail.com>
2019-08-15 21:47:49 -0400

    Initial Commit

---
+++ prac/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prac
+//
+//  Created by Saqib Malik on 8/15/19.
+//  Copyright © 2019 Saqib Malik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pracTests/pracTests.swift
@@ -0,0 +1,34 @@
+//
+//  pracTests.swift
+//  pracTests
+//
+//  Created by Saqib Malik on 8/15/19.
+//  Copyright © 2019 Saqib Malik. All rights reserved.
+//
+
+import XCTest
+@testable import prac
+
+class pracTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
